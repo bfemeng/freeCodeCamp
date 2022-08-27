2e0b72488489f90a5b076804b890e7ebe30f87e1
bfemeng <92241705+bfemeng@users.noreply.github.com>
2022-08-26 20:33:31 -0400

    cc

--- practice.js
+++ practice.js
@@ -3,3 +3,9 @@ for (i = 0; i < 10; i++) {
     placeTile('orange')
     placeTile('mint')
   }
+
+  // Change the text between the quotation marks and run your code to see the name of your favorite language animated!
+drawText('Javascript');
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
