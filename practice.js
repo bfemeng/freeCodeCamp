92c39c600f732b0ccdbe81d6c00153be6f7f66ea
bfemeng <92241705+bfemeng@users.noreply.github.com>
2022-08-26 20:56:42 -0400

    cca

--- practice.js
+++ practice.js
@@ -9,3 +9,11 @@ drawText('Javascript');
 
 // The code below will allow you to interact with the letters
 bounceBubbles();
+
+# We have hidden code in another file. If you're curious, open the folder to the left and inspect the simulate.py file
+
+from simulate import simulate
+
+num_people_in_room = 22 			#Change This Number (keep it smaller than 100 to save processing power)
+
+simulate(num_people_in_room) 
